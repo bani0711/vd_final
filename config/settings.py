9783074b1464cdad9c19ccc8064141d1d097740c
bani0711 <bani0711@naver.com>
2020-06-25 20:55:04 +0900

    세팅 수정

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(j2*=heyt^wat403gsxbvf#))#)k4%7u+)s7*g!$zo8142&)*1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bani07111.pythonanywhere.com']
 
 
 # Application definition
